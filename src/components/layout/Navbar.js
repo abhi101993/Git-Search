23ea40da90d3daf00ddb36f3fabc22338fdcd578
Abhishek <abhi101993@gmail.com>
2021-03-02 20:51:05 +0530

    initial commit v1

---
+++ src/components/layout/Navbar.js
@@ -0,0 +1,34 @@
+import PropTypes from 'prop-types';
+
+
+const Navbar = ({icon,title}) =>
+{
+
+    return (
+        <navbar className="navbar bg-primary">
+       
+       <h1>
+       <i className={icon}></i> {title}
+       </h1>
+       </navbar>
+   );
+}
+
+Navbar.defaultProps = 
+{
+
+ title:"Github Finder",
+ icon:"fab fa-github"
+
+};
+
+Navbar.propTypes =
+{
+    title:PropTypes.string.isRequired,
+    icon:PropTypes.string.isRequired
+
+};
+
+
+
+export default Navbar;
