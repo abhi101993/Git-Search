f7167f328a5899b3a67742bd2af9ab4994b15acb
Abhishek <abhi101993@gmail.com>
2021-03-04 00:24:52 +0530

    Add Repo page

--- src/App.js
+++ src/App.js
@@ -1,16 +1,22 @@
-import React, {Component} from 'react';
+import React, {Component, Fragment} from 'react';
+import {BrowserRouter as Router,Switch,Route} from 'react-router-dom';
 import './App.css';
 import Navbar from  './components/layout/Navbar';
 import Users from './components/users/Users';
+import User from './components/users/User';
 import Search from  './components/users/Search';
 import axios from 'axios';
-
+import Alert from  './components/layout/Alert';
+import About from './components/pages/About';
 class App extends Component {
  
   state = 
   {
     users:[],
-    loading:false
+    repos:[],
+    user: {},
+    loading:false,
+    alert:null
   };
 
 
@@ -36,28 +42,71 @@ class App extends Component {
     &client_secret=${process.env.REACT_APP_GITHUB_CLIENT_SECRETS}`
     );
 
-    this.setState({user:res.data.items,loading:false});
+    this.setState({users:res.data.items,loading:false});
     
   };
 
+  getUser = async (username) =>
+  {
+    this.setState({loading:true});
+
+    const res = await axios.get(`https://api.github.com/users/${username}?client_id=${process.env.REACT_APP_GITHUB_CLIENT_ID}
+    &client_secret=${process.env.REACT_APP_GITHUB_CLIENT_SECRETS}`);
+
+    this.setState({user:res.data,loading:false});
+  }
+
+  getUserRepos = async username =>
+  {
+    this.setState({loading:true});
+
+    const res = await axios.get(`https://api.github.com/users/${username}/repos?
+    per_page=5&sort=created:asc&client_id=${process.env.REACT_APP_GITHUB_CLIENT_ID}&client_secret=${process.env.REACT_APP_GITHUB_CLIENT_SECRETS}`);
+
+    this.setState({repos:res.data,loading:false});
+  }
+
   clearUsers = () => this.setState({users:[],loading:false});
   
+  setAlert = (msg,type) =>
+  {
+    this.setState({alert:{msg: msg,type: type}});
+
+    setTimeout(() => {
+      this.setState({alert:null})
+    }, 5000);
+  };
   
    
   render()
   {
-    const {users,loading} = this.state;
+    const {users,loading,user,repos} = this.state;
 
     return (
 
+
+       <Router>
        <div className="App">
         <Navbar/>
         <div className='container'>
-        <Search searchUsers={this.searchUsers} clearUsers={this.clearUsers} showClear={
-        users.length>0?true:false}/>
+        <Alert alert={this.state.alert}/>
+        <Switch>
+          <Route exact path='/' render={props => (
+           <Fragment>
+            <Search searchUsers={this.searchUsers} clearUsers={this.clearUsers} showClear={
+            users.length>0?true:false} setAlert={this.setAlert} />
         <Users loading={loading} users={users} />
+           </Fragment>
+          )} />
+          <Route exact path ='/about'  component={About}/>
+          <Route exact path='/user/:login' render={props => (
+            <User {...props} getUser={this.getUser} user={user} loading={loading}
+             getUserRepos={this.getUserRepos} repos={repos}/>
+          )} />
+        </Switch>
         </div>
        </div>
+       </Router>
     
     );
       
--- src/components/users/Search.js
+++ src/components/users/Search.js
@@ -1,27 +1,40 @@
 import React, { Component } from 'react'
 import PropTypes from 'prop-types'
 
-export class Search extends Component {
+class Search extends Component {
 
-    state =
+     state =
     {
         text: ''
     };
 
-   static propTypes =
+ static  propTypes =
     {
         searchUsers: PropTypes.func.isRequired,
         clearUsers: PropTypes.func.isRequired,
-        showClear: PropTypes.bool.isRequired
+        showClear: PropTypes.bool.isRequired,
+        setAlert: PropTypes.func.isRequired
 
     };
     
     onSubmit = e => {
         e.preventDefault();
 
-        this.props.searchUsers(this.state.text);
+        if(this.state.text ==='')
+        {
+            this.props.setAlert('Please Enter Something','light');
+        }
 
-        this.setState({text:''});
+        else
+        {
+
+            this.props.searchUsers(this.state.text);
+
+            this.setState({text:''});
+
+        }
+
+        
     }
 
     onChange = e =>  this.setState({[e.target.name]:e.target.value});
